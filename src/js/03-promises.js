890308258852325122d9a08446bdf49c3af9bff8
BartekJedliczka <bartekjedliczka@gmail.com>
2022-11-05 20:12:47 +0100

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,9 @@
+import Notiflix from 'notiflix';
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const btnCreateProimses = document.querySelector('button[type="submit"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
